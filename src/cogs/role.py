58a79a20a1729ce79f604bf87c677ce21768170b
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-10-22 02:32:59 +0200

    Update role.py
    added downtimes

--- src/cogs/role.py
+++ src/cogs/role.py
@@ -4,6 +4,20 @@ class Role(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
 
+  @commands.Cog.listener('on_ready')
+  async def roles(self):
+    with open("data/role.json", 'r') as f:
+      data = json.load(f)
+    tasks: list = []
+    for guild in self.bot.guilds:
+      try:
+        role = guild.get_role(int(data[str(guild.id)]))
+      except:
+        pass
+      for member in guild.members:
+        if not role in member.roles: tasks.append(asyncio.create_task(member.add_roles(role)))
+    for i in tasks: await i
+
   @commands.Cog.listener('on_member_join')
   async def role(self, member):
     with open("data/role.json", 'r') as f:
